91f45c44d82d1cf945c34e5c44973a142106ad6a
Rahul <chrahulofficial@gmail.com>
2025-05-03 19:29:18 +0530

    done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const uploadimg = document.querySelector("#uploadimg");
 const uploadimgbtn = document.querySelector("#uploadimgbtn");
 const previewimg = document.querySelector("#preview");
+const preview = document.querySelector("#previewimages");
 const previewTxt = document.querySelector("#previewTxt");
 
 uploadimgbtn.addEventListener("click", () => {
@@ -10,9 +11,11 @@ uploadimgbtn.addEventListener("click", () => {
     const files = uploadimg.files;
     for (let file of files) {
       const imgURL = URL.createObjectURL(file);
-      console.log(imgURL);
-      previewimg.src = imgURL
-      previewTxt.classList.add("hidden")
+      previewTxt.classList.add("hidden");
+      const imgPreview = document.createElement("img");
+      imgPreview.src = imgURL;
+      imgPreview.className = "w-32 h-32 object-cover m-2 rounded-md";
+      preview.appendChild(imgPreview);
     }
   });
 });
