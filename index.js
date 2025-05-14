a58b4a700ef24335f21d7c97c98b65d4d49018ed
Rahul <chrahulofficial@gmail.com>
2025-05-14 22:37:45 +0530

    done

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const previewTxt = document.querySelector("#previewTxt");
 const disableBtn = document.querySelector("#disableBtn");
 const downloadBtn = document.getElementById("downloadBtn");
 const canvas = document.getElementById("mergeCanvas");
+const uploadbtnimage = document.getElementById("uploadbtnimage");
+const uploadbtnpdf = document.getElementById("uploadbtnpdf");
 const ctx = canvas.getContext("2d");
+const uploadImage = document.getElementById("uploadImage");
+const uploadPDF = document.getElementById("uploadPDF");
 
 uploadimgbtn.addEventListener("click", () => {
   uploadimg.click();
@@ -14,6 +18,7 @@ uploadimgbtn.addEventListener("click", () => {
 uploadimg.addEventListener("change", () => {
   const files = uploadimg.files;
   preview.innerHTML = "";
+
   if (files.length === 0) {
     previewTxt.classList.remove("hidden");
     downloadBtn.classList.add("hidden");
@@ -26,11 +31,13 @@ uploadimg.addEventListener("change", () => {
   disableBtn.classList.add("hidden");
 
   for (let file of files) {
-    const imgURL = URL.createObjectURL(file);
-    const imgPreview = document.createElement("img");
-    imgPreview.src = imgURL;
-    imgPreview.className = "object-cover m-2 rounded-md w-52 h-52";
-    preview.appendChild(imgPreview);
+    const divEle = document.createElement("div");
+    const pTag = document.createElement("p");
+    pTag.innerHTML = file.name;
+    divEle.classList =
+      "px-8 py-2.5 rounded-lg shadow-xl shadow-gray-800 cursor-pointer text-sm w-28 bg-white";
+    divEle.appendChild(pTag);
+    preview.appendChild(divEle);
   }
 });
 
@@ -50,8 +57,8 @@ downloadBtn.addEventListener("click", async () => {
 
   const cols = 3;
   const rows = Math.ceil(images.length / cols);
-  const imgWidth = 500;
-  const imgHeight = 500;
+  const imgWidth = 600;
+  const imgHeight = 600;
 
   canvas.width = cols * imgWidth;
   canvas.height = rows * imgHeight;
@@ -69,5 +76,20 @@ downloadBtn.addEventListener("click", async () => {
   link.click();
 });
 
+uploadbtnimage.addEventListener("click", () => {
+  if (uploadbtnpdf.classList.contains("bg-[#f1f5f9]")) {
+    uploadbtnpdf.classList.remove("bg-[#f1f5f9]");
+  }
+  uploadbtnimage.classList.add("bg-[#f1f5f9]");
+  uploadImage.classList.remove("hidden");
+  uploadPDF.classList.add("hidden");
+});
 
-
+uploadbtnpdf.addEventListener("click", () => {
+  if (uploadbtnimage.classList.contains("bg-[#f1f5f9]")) {
+    uploadbtnimage.classList.remove("bg-[#f1f5f9]");
+  }
+  uploadbtnpdf.classList.add("bg-[#f1f5f9]");
+  uploadPDF.classList.remove("hidden");
+  uploadImage.classList.add("hidden");
+});
